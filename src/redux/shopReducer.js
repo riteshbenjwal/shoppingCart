907a94edc97477cb5938e24fdafb78138ef66f23
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-28 15:23:06 +0530

    FINISHED

--- src/redux/shopReducer.js
+++ src/redux/shopReducer.js
@@ -1,41 +1,35 @@
 import initialState from "./initialState";
 import * as actionTypes from "./actions";
 
-const shopReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actionTypes.ADD_TO_CART:
-      const item = state.products.find(
-        (product) => product.id == action.payload.id
-      );
-      const inCart = state.cart.find((product) =>
-        product.id == action.payload.id ? true : false
-      );
-
-      return {
-        ...state,
-        cart: inCart
-          ? state.cart.map((product) =>
-              product.id == action.payload.id
-                ? {
-                    ...product,
-                    qty: product.qty + 1,
-                  }
-                : product
-            )
-          : [...state.cart, { ...item, qty: 1 }],
-      };
-
-
-      case actionTypes.LOAD_CURRENT_ITEM:
+const shopReducer = (state=initialState,action) => {
+  switch(action.type){
+      case actionTypes.ADD_TO_CART : 
+          const item = state.products.find((product)=>product.id==action.payload.id)
+          const inCart = state.cart.find((product)=>product.id==action.payload.id?true:false)
           return{
               ...state,
-              currentItem: action.payload.item
+              cart : inCart ?
+                      state.cart.map((product)=>product.id==action.payload.id?{...product,qty:product.qty+1}:product) : 
+                      [...state.cart,{...item,qty:1}]
+          }
+      case actionTypes.LOAD_CURRENT_ITEM : 
+          return {
+              ...state,
+              currentItem : action.payload.item
           }
 
-
+      case actionTypes.UPDATE_QTY : 
+          return{
+              ...state,
+              cart : state.cart.map((product)=>product.id==action.payload.id?{...product,qty:action.payload.qty}:product)
+          }
+      case actionTypes.REMOVE_FROM_CART : 
+          return{
+              ...state,
+              cart : state.cart.filter((product)=>product.id!=action.payload.id)
+          }
+      default : return state
   }
+}
 
-  return state;
-};
-
-export default shopReducer;
+export default shopReducer
